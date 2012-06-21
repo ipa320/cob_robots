9f46707162b81ed6cbaa4fe321813a9af04c9ab7
ipa-fmw-ms <max-sieber@gmx.de>
2012-06-21 19:41:38 +0200

    added base light and sound tests

---
+++ cob_hardware_test/src/cob_hardware_test/srv/__init__.py
@@ -0,0 +1,1 @@
+from ._Dialog import *
---
+++ cob_hardware_test/src/dialog.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('cob_bringup')
+import sys
+import rospy
+import wx
+from cob_bringup.srv import *
+
+def handle_dialog(req):
+    if req.type == 1:
+	    print "Asking: %s" % (req.message)
+	    ex = wx.App()
+	    dial = wx.MessageDialog(None, req.message, 'Question',
+			    wx.YES_NO | wx.ICON_QUESTION)
+	    ret = dial.ShowModal()
+	    if ret == wx.ID_YES:
+		    answer = True
+	    else:
+		    answer = False
+
+	#TODO exit properly
+	#self.Destroy()
+	#ex.Destroy()	
+	#dial.Destroy()
+	    return DialogResponse(answer)
+
+    if req.type == 0:
+	    print "Confirm: %s" % (req.message)
+	    ex = wx.App()
+	    dial = wx.MessageDialog(None, req.message, 'Confirm',
+			    wx.OK | wx.ICON_WARNING)
+	    ret = dial.ShowModal()
+	    if ret == wx.ID_OK:
+		    answer = True
+	    else:
+		    answer = False
+
+	#TODO exit properly
+	#self.Destroy()
+	#ex.Destroy()	
+	#dial.Destroy()
+	    return DialogResponse(answer)
+
+
+
+def dialog_server():
+	rospy.init_node('dialog_server')
+	s = rospy.Service('dialog', Dialog, handle_dialog)
+	print "Ready to ask(1) or confirm(0)!"
+	rospy.spin()
+
+
+
+if __name__ == "__main__":
+      dialog_server()
