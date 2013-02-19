97a56b840b5c76506d2d26c81b2b676e2dda66dd
ipa-fmw-ms <max-sieber@gmx.de>
2013-02-19 15:33:35 +0100

    copied rospy.wait(.75) from hztest and replaced tabs with spaces in all python files

--- cob_hardware_test/test/base_test.py
+++ cob_hardware_test/test/base_test.py
@@ -15,59 +15,58 @@ from pr2_controllers_msgs.msg import *
 
 def dialog_client(dialog_type, message):
     #dialog type: 0=confirm 1=question
-	rospy.wait_for_service('dialog')
-	try:
-		dialog = rospy.ServiceProxy('dialog', Dialog)
-		resp1 = dialog(dialog_type, message)
-		return resp1.answer
-	except rospy.ServiceException, e:
-		print "Service call failed: %s" % e
+    rospy.wait_for_service('dialog')
+    try:
+        dialog = rospy.ServiceProxy('dialog', Dialog)
+        resp1 = dialog(dialog_type, message)
+        return resp1.answer
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
 
 class HardwareTest(unittest.TestCase):
-	def __init__(self, *args):
+    def __init__(self, *args):
 
-		super(HardwareTest, self).__init__(*args)
-		rospy.init_node('test_hardware_test')
-		torso_joint_states = []
-		self.message_received = False
-		self.sss = simple_script_server()
+        super(HardwareTest, self).__init__(*args)
+        rospy.init_node('test_hardware_test')
+        torso_joint_states = []
+        self.message_received = False
+        self.sss = simple_script_server()
 
-		try:
-				# move_
-				if not rospy.has_param('~move_x'):
-					self.fail('Parameter move_x does not exist on ROS Parameter Server')
-				self.move_x = rospy.get_param('~move_x')
+        try:
+                # move_
+                if not rospy.has_param('~move_x'):
+                    self.fail('Parameter move_x does not exist on ROS Parameter Server')
+                self.move_x = rospy.get_param('~move_x')
 
-				if not rospy.has_param('~move_y'):
-					self.fail('Parameter move_ does not exist on ROS Parameter Server')
-				self.move_y = rospy.get_param('~move_y')
+                if not rospy.has_param('~move_y'):
+                    self.fail('Parameter move_ does not exist on ROS Parameter Server')
+                self.move_y = rospy.get_param('~move_y')
 
-				if not rospy.has_param('~move_theta'):
-					self.fail('Parameter move_ does not exist on ROS Parameter Server')
-				self.move_theta = rospy.get_param('~move_theta')
+                if not rospy.has_param('~move_theta'):
+                    self.fail('Parameter move_ does not exist on ROS Parameter Server')
+                self.move_theta = rospy.get_param('~move_theta')
 
-		except KeyError, e:
-				self.fail('Parameters not set properly')
+        except KeyError, e:
+                self.fail('Parameters not set properly')
 
 
-	def test_base(self):
-		self.sss.init("base")
-		self.assertTrue(dialog_client(0, 'Ready to move base?' ))
-		handle = self.sss.move_base_rel("base", [self.move_x, self.move_y, self.move_theta])
-		self.assertEqual(handle.get_state(), 3)
-		self.assertTrue(dialog_client(1, 'Did I move?'))
-		self.assertTrue(dialog_client(0, 'EM Pressed and Released? \n Ready to move my Base ?' ))
-		self.sss.recover("base")
-		handle = self.sss.move_base_rel("base", [self.move_x, self.move_y, self.move_theta])
-
-		
-
-		
+    def test_base(self):
+        self.sss.init("base")
+        self.assertTrue(dialog_client(0, 'Ready to move base?' ))
+        handle = self.sss.move_base_rel("base", [self.move_x, self.move_y, self.move_theta])
+        self.assertEqual(handle.get_state(), 3)
+        self.assertTrue(dialog_client(1, 'Did I move?'))
+        self.assertTrue(dialog_client(0, 'EM Pressed and Released? \n Ready to move my Base ?' ))
+        self.sss.recover("base")
+        handle = self.sss.move_base_rel("base", [self.move_x, self.move_y, self.move_theta])
 
 if __name__ == '__main__':
-
-	try:
-		rostest.run('rostest', 'test_hardware_test', HardwareTest, sys.argv)
-	except KeyboardInterrupt, e:
-		pass
-	print "exiting"
+    # Copied from hztest: A dirty hack to work around an apparent race condition at startup
+    # that causes some hztests to fail. Most evident in the tests of
+    # rosstage.
+    time.sleep(0.75)
+    try:
+        rostest.run('rostest', 'test_hardware_test', HardwareTest, sys.argv)
+    except KeyboardInterrupt, e:
+        pass
+    print "exiting"
--- cob_hardware_test/test/sound_test.py
+++ cob_hardware_test/test/sound_test.py
@@ -14,43 +14,43 @@ from pr2_controllers_msgs.msg import *
 
 
 def dialog_client(dialog_type, message):
-	#dialog type: 0=confirm 1=question
-	rospy.wait_for_service('dialog')
-	try:
-		dialog = rospy.ServiceProxy('dialog', Dialog)
-		resp1 = dialog(dialog_type, message)
-		return resp1.answer
-	except rospy.ServiceException, e:
-		print "Service call failed: %s" % e
+    #dialog type: 0=confirm 1=question
+    rospy.wait_for_service('dialog')
+    try:
+        dialog = rospy.ServiceProxy('dialog', Dialog)
+        resp1 = dialog(dialog_type, message)
+        return resp1.answer
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
 
 class HardwareTest(unittest.TestCase):
-	def __init__(self, *args):
+    def __init__(self, *args):
 
-		super(HardwareTest, self).__init__(*args)
-		rospy.init_node('test_hardware_test')
-		torso_joint_states = []
-		self.message_received = False
-		self.sss = simple_script_server()
+        super(HardwareTest, self).__init__(*args)
+        rospy.init_node('test_hardware_test')
+        torso_joint_states = []
+        self.message_received = False
+        self.sss = simple_script_server()
 
-	def test_play(self):
-		dialog_client(0, 'Listen up for the sound' )
-		rospy.set_param("script_server/sound/language","de")
-		handle = self.sss.play("grasp_tutorial_01")
-		self.assertEqual(handle.get_state(), 3)
-		self.assertTrue(dialog_client(1, 'Did you hear some audio file?'))
-		
-	def test_say(self):
-		dialog_client(0, 'Listen up for the sound' )
-		handle = self.sss.say(["Hello"])
-		self.assertEqual(handle.get_state(), 3)
-		if handle.get_error_code() != 0:
-			error_msg = 'Could say something'
-			self.fail(error_msg + "; errorCode: " + str(handle.get_error_code()))
-		self.assertTrue(dialog_client(1, 'Did you hear <<Hello>>?'))
+    def test_play(self):
+        dialog_client(0, 'Listen up for the sound' )
+        rospy.set_param("script_server/sound/language","de")
+        handle = self.sss.play("grasp_tutorial_01")
+        self.assertEqual(handle.get_state(), 3)
+        self.assertTrue(dialog_client(1, 'Did you hear some audio file?'))
+        
+    def test_say(self):
+        dialog_client(0, 'Listen up for the sound' )
+        handle = self.sss.say(["Hello"])
+        self.assertEqual(handle.get_state(), 3)
+        if handle.get_error_code() != 0:
+            error_msg = 'Could say something'
+            self.fail(error_msg + "; errorCode: " + str(handle.get_error_code()))
+        self.assertTrue(dialog_client(1, 'Did you hear <<Hello>>?'))
 
 if __name__ == '__main__':
-	try:
-		rostest.run('rostest', 'test_hardware_test', HardwareTest, sys.argv)
-	except KeyboardInterrupt, e:
-		pass
-	print "exiting"
+    try:
+        rostest.run('rostest', 'test_hardware_test', HardwareTest, sys.argv)
+    except KeyboardInterrupt, e:
+        pass
+    print "exiting"
