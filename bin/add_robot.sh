b8038db7223f7a69ea02cd432fc5b7a236634e31
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2013-04-09 11:15:50 +0200

    added cob_moveit_config

---
+++ bin/add_robot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+robot=$1
+
+base=`rospack find cob_moveit_config`
+mkdir -p $base/$robot/config
+
+cat <<SRDF_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/$robot/config/robot.srdf
+<robot name="ROBOT">
+</robot>
+SRDF_TEMPLATE
+
+cat <<CONFIG_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/$robot/.setup_assistant
+moveit_setup_assistant_config:
+  URDF:
+    package: cob_hardware_config
+    relative_path: ROBOT/urdf/ROBOT.urdf.xacro
+  SRDF:
+    relative_path: config/robot.srdf
+CONFIG_TEMPLATE
