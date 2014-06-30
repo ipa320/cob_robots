c72e70aec3ff13e6f9baf84deace1e351716f8c7
ipa-fxm <felix.messmer@ipa.fhg.de>
2014-06-30 11:28:14 +0200

    test_publisher for controller tuning

--- cob_controller_configuration_gazebo/scripts/test_publisher_vel_control.py
+++ cob_controller_configuration_gazebo/scripts/test_publisher_vel_control.py
@@ -6,21 +6,28 @@ import math
 from brics_actuator.msg import JointVelocities, JointValue
  
 def velPub():
-  rospy.init_node("velPub",anonymous=True)
+  rospy.init_node("velPub", anonymous=True)
   
-  pub = rospy.Publisher("/arm_controller/command_vel", JointVelocities, queue_size=10)
+  component_name = "arm"
+  component_dof = 7
+  
+  pub = rospy.Publisher("/"+component_name+"_controller/command_vel", JointVelocities, queue_size=1)
   vel =  JointVelocities()
-  for x in range (0,7):
+  for x in range (0,component_dof):
     vel.velocities.append(JointValue())
   
   r = rospy.Rate(100)
-  ini_time = rospy.get_time()
+  t0 = rospy.get_time()
+  A = 0.1 #amplitude
+  w = 1.0 #frequency
+  phi = 0.0 #phase shift
+  
   
   while not rospy.is_shutdown():
-    #current_time = rospy.get_time() - ini_time
-    #vel.velocities[1].value=math.sin(current_time/3)/20
+    t = rospy.get_time()
+    vel.velocities[1].value=A*math.sin(w*(t-t0)+phi)
     
-    print vel
+    #print vel
     pub.publish(vel)
     
     r.sleep()
