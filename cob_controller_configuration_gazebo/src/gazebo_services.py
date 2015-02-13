4dd5c7c54adfc18b60cf1494cebcc96f2f857a75
ipa-fxm <felix.messmer@ipa.fhg.de>
2015-02-13 08:18:04 +0100

    more namespace adjustments for cob3-6 simulation

--- cob_controller_configuration_gazebo/src/gazebo_services.py
+++ cob_controller_configuration_gazebo/src/gazebo_services.py
@@ -12,11 +12,11 @@ from cob_srvs.srv import *
 class gazebo_services():
 
 	def __init__(self):
-		self.action_client = actionlib.SimpleActionClient('follow_joint_trajectory', FollowJointTrajectoryAction)
-		self.init_srv = rospy.Service('init', Trigger, self.init_cb)
-		self.stop_srv = rospy.Service('stop', Trigger, self.stop_cb)
-		self.recover_srv = rospy.Service('recover', Trigger, self.recover_cb)
-		self.set_operation_mode_srv = rospy.Service('set_operation_mode', SetOperationMode, self.set_operation_mode_cb)
+		self.action_client = actionlib.SimpleActionClient('joint_trajectory_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
+		self.init_srv = rospy.Service('driver/init', Trigger, self.init_cb)
+		self.stop_srv = rospy.Service('driver/stop', Trigger, self.stop_cb)
+		self.recover_srv = rospy.Service('driver/recover', Trigger, self.recover_cb)
+		self.set_operation_mode_srv = rospy.Service('driver/set_operation_mode', SetOperationMode, self.set_operation_mode_cb)
 
 	def init_cb(self, req):
 		resp = TriggerResponse()
