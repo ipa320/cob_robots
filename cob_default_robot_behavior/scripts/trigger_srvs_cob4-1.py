4f21554829f412eb6201afb0a305203e4659380d
ipa-tif <tim.froehlich@ipa.fraunhofer.de>
2016-06-29 18:12:25 +0200

    Positions for head and torso

--- cob_default_robot_behavior/scripts/trigger_srvs_cob4-1.py
+++ cob_default_robot_behavior/scripts/trigger_srvs_cob4-1.py
@@ -66,6 +66,9 @@ sss = simple_script_server()
 from std_msgs.msg import ColorRGBA
 from std_srvs.srv import Trigger, TriggerResponse
 
+base_th = None
+torso_vel = None
+
 def torso_front_cb(req):
     sss.move_base_rel("base",[0,0,1.57],False)
     sss.move("torso","front_down_full",True)
@@ -75,6 +78,215 @@ def front_to_home_cb(req):
     sss.move_base_rel("base",[0,0,-1.57],False)
     sss.move("torso","home",False)
     return TriggerResponse(True, "")
+
+
+#Torso movements for MSH Szenario
+#################################################################
+def T0_cb(req): #Normalzustand
+    global torso_vel
+    if torso_vel != None:
+        rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    else:
+        torso_vel = 0.5        
+        rospy.set_param("/script_server/torso/default_vel", torso_vel)
+        
+    if base_th != None:
+        rospy.loginfo("turning back")
+        sss.move_base_rel("base",[0,0,-base_th],False)
+
+    sss.move("torso","home",True)
+    rospy.set_param("/script_server/torso/default_vel", 0.5)
+    return TriggerResponse(True, "")
+
+def T1_cb(req): #Schraeglage nach Rechts
+    global base_th
+    global torso_vel
+    base_th = 1.57
+    torso_vel = 0.35
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[0.6, -1.57]],True)
+    return TriggerResponse(True, "")
+
+def T2_cb(req): #Schraeglage nach Links
+    global base_th
+    global torso_vel
+    base_th = 1.57
+    torso_vel = 0.35
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[-0.6,-1.57]],True)
+    return TriggerResponse(True, "")
+
+def T3_cb(req): #Nach Vorne
+    global base_th
+    global torso_vel
+    base_th = 0.5
+    torso_vel = 0.6
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[-1.0, 0.5]],True)
+    return TriggerResponse(True, "")
+
+def T4_cb(req):#Nach Hinten
+    global base_th
+    global torso_vel
+    base_th = -0.5
+    torso_vel = 0.6
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[1.0, -0.5]],True)
+    return TriggerResponse(True, "")
+
+def T5_cb(req):#Kreisend Uhrzeigersinn
+    global base_th
+    global torso_vel
+    base_th = -1.57
+    torso_vel = 0.3
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move("torso",[[-1.0, 2.5]],False)
+    sss.move_base_rel("base",[0,0,base_th],True)
+    sss.move_base_rel("base",[0,0,-base_th],False)
+    sss.move("torso",[[0.8, -0.5]],True)
+    sss.move("torso","home",True)
+    base_th = None
+    return TriggerResponse(True, "")
+
+def T6_cb(req):#Kreisend gegen Uhrzeigersinn
+    global base_th
+    global torso_vel
+    base_th = 1.57
+    torso_vel = 0.3
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move("torso",[[1.0, -2.5]],False)
+    sss.move_base_rel("base",[0,0,base_th],True)
+    sss.move_base_rel("base",[0,0,-base_th],False)
+    sss.move("torso",[[-0.8, 0.5]],True)
+    sss.move("torso","home",False)
+    base_th = None
+    return TriggerResponse(True, "")
+
+def T7_cb(req):#Schraeglage Rechts-Vorne
+    global base_th
+    global torso_vel
+    base_th = -0.3
+    torso_vel = 0.4
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[-0.6, 0.88]],True)
+    return TriggerResponse(True, "")
+
+def T8_cb(req):#Schraeglage Links-Vorne
+    global base_th
+    global torso_vel
+    base_th = 0.3
+    torso_vel = 0.4
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[-0.6, 0]],False)
+    return TriggerResponse(True, "")
+
+def T9_cb(req):#Schraeglage Rechts-Hinten
+    global base_th
+    global torso_vel
+    base_th = 0.3
+    torso_vel = 0.4
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[0.6, -0.8]],False)
+    return TriggerResponse(True, "")
+
+def T10_cb(req):#Schraeglage Links-Hinten
+    global base_th
+    global torso_vel
+    base_th = -0.5
+    torso_vel = 0.4
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[0.4, 0.0]],False)
+    return TriggerResponse(True, "")
+
+def T11_cb(req):#Anzeige
+    global base_th
+    global torso_vel
+    base_th = 0.5
+    torso_vel = 0.4
+    rospy.set_param("/script_server/torso/default_vel", torso_vel)
+    sss.move_base_rel("base",[0,0,base_th],False)
+    sss.move("torso",[[0.4, -0.25]],False)
+    return TriggerResponse(True, "")
+################################################################
+
+
+#Head movements for MSH Szenario
+#################################################################
+def K0_cb(req):#Normalzustand
+    rospy.set_param("/script_server/head/default_vel", 0.7)
+    sss.move("head","home",True)
+    return TriggerResponse(True, "")
+
+def K1_cb(req):#Schraeglage nach Rechts
+    rospy.set_param("/script_server/head/default_vel", 0.7)
+    sss.move("head",[[ -1.4, -0.5, 2]], True)
+    return TriggerResponse(True, "")
+
+def K2_cb(req):#Schraeglage nach Links
+    rospy.set_param("/script_server/head/default_vel", 0.7)
+    sss.move("head",[[ 1.8,-0.5,-1.26]], True)
+    return TriggerResponse(True, "")
+
+def K3_cb(req):#Nach vorne unten
+    rospy.set_param("/script_server/head/default_vel", 0.7)
+    sss.move("head",[[ 0.5, -1.0, 0.5]], True)
+    return TriggerResponse(True, "")
+
+def K4_cb(req):#Nach hinten
+    rospy.set_param("/script_server/head/default_vel", 0.7)
+    sss.move("head",[[ 1.57, 0, 1.57]], True)
+    return TriggerResponse(True, "")
+
+def K5_cb(req):#Kreisend im Uhrzeigersinn
+    rospy.set_param("/script_server/head/default_vel", 0.75)
+    sss.move("head",[[-6.28,0.6,6.28]], True)
+    sss.move("head","home", True)
+    return TriggerResponse(True, "")
+
+def K6_cb(req):#Kreisend gegen Uhrzeigersinn
+    rospy.set_param("/script_server/head/default_vel", 0.75)
+    sss.move("head",[[6.28,-0.6,-6.28]], True)
+    sss.move("head","home", True)
+    return TriggerResponse(True, "")
+
+def K7_cb(req):#Schraeglage Rechts-Vorne
+    rospy.set_param("/script_server/head/default_vel", 0.6)
+    sss.move("head",[[-0.4,-0.8,1.4]], True)
+    return TriggerResponse(True, "")
+
+
+def K8_cb(req):#Schraeglage Links-Vorne
+    rospy.set_param("/script_server/head/default_vel", 0.6)
+    sss.move("head",[[1.4,-0.8,-0.4]], True)
+    return TriggerResponse(True, "")
+
+def K9_cb(req):#Schraeglage Rechts-Hinten
+    rospy.set_param("/script_server/head/default_vel", 0.6)
+    sss.move("head",[[0.4,0.8,-1.0]], True)
+    return TriggerResponse(True, "")
+
+def K10_cb(req):#Schraeglage Links-Hinten
+    rospy.set_param("/script_server/head/default_vel", 0.6)
+    sss.move("head",[[-1.4,0.8,0.4]], True)
+    return TriggerResponse(True, "")
+
+def K11_cb(req):
+    sss.move("head","back",False)
+    return TriggerResponse(True, "")
+
+def K12_cb(req):
+    rospy.set_param("/script_server/head/default_vel", 0.6)
+    sss.move("head",[[1.51, -3.14, -0.5]], True)
+    return TriggerResponse(True, "")
+################################################################
 
 def setLightCyan_cb(req):
     sss.set_light("light_base","cyan")
@@ -186,9 +398,39 @@ def soundHello_cb(req):
     return TriggerResponse(True, "")
 
 def trigger_srvs():
-    rospy.init_node('trigger_srvs')
     s = rospy.Service('/behavior/torso_front', Trigger, torso_front_cb)
     s = rospy.Service('/behavior/torso_front_home', Trigger, front_to_home_cb)
+    #Torso movements for MSH Szenario
+    #################################################################
+    s = rospy.Service('/behavior/T0', Trigger, T0_cb)
+    s = rospy.Service('/behavior/T1', Trigger, T1_cb)
+    s = rospy.Service('/behavior/T2', Trigger, T2_cb)
+    s = rospy.Service('/behavior/T3', Trigger, T3_cb)
+    s = rospy.Service('/behavior/T4', Trigger, T4_cb)
+    s = rospy.Service('/behavior/T5', Trigger, T5_cb)
+    s = rospy.Service('/behavior/T6', Trigger, T6_cb)
+    s = rospy.Service('/behavior/T7', Trigger, T7_cb)
+    s = rospy.Service('/behavior/T8', Trigger, T8_cb)
+    s = rospy.Service('/behavior/T9', Trigger, T9_cb)
+    s = rospy.Service('/behavior/T10', Trigger, T10_cb)
+    s = rospy.Service('/behavior/T11', Trigger, T11_cb)
+     #################################################################
+    #Head movements for MSH Szenario
+    #################################################################
+    s = rospy.Service('/behavior/K0', Trigger, K0_cb)
+    s = rospy.Service('/behavior/K1', Trigger, K1_cb)
+    s = rospy.Service('/behavior/K2', Trigger, K2_cb)
+    s = rospy.Service('/behavior/K3', Trigger, K3_cb)
+    s = rospy.Service('/behavior/K4', Trigger, K4_cb)
+    s = rospy.Service('/behavior/K5', Trigger, K5_cb)
+    s = rospy.Service('/behavior/K6', Trigger, K6_cb)
+    s = rospy.Service('/behavior/K7', Trigger, K7_cb)
+    s = rospy.Service('/behavior/K8', Trigger, K8_cb)
+    s = rospy.Service('/behavior/K9', Trigger, K9_cb)
+    s = rospy.Service('/behavior/K10', Trigger, K10_cb)
+    s = rospy.Service('/behavior/K11', Trigger, K11_cb)
+    s = rospy.Service('/behavior/K12', Trigger, K12_cb)
+     #################################################################
     s = rospy.Service('/behavior/setLightCyan', Trigger, setLightCyan_cb)
     s = rospy.Service('/behavior/setLightRed', Trigger, setLightRed_cb)
     s = rospy.Service('/behavior/setLightGreen', Trigger, setLightGreen_cb)
@@ -207,7 +449,10 @@ def trigger_srvs():
     s = rospy.Service('/behavior/soundStarting',Trigger,soundStarting_cb)
     s = rospy.Service('/behavior/soundHello',Trigger,soundHello_cb)
 
-    rospy.spin()
 
 if __name__ == "__main__":
+    rospy.init_node('trigger_srvs')
     trigger_srvs()
+    rospy.loginfo("Trigger services up and running!")
+    
+    rospy.spin()
