cfb822331947f6343368397eb28034a90cab53a0
ipa-fxm <felix.messmer@ipa.fhg.de>
2017-04-05 19:06:46 +0200

    restructure cob_hardware_config

--- cob_moveit_config/bin/add_robot.sh
+++ cob_moveit_config/bin/add_robot.sh
@@ -14,7 +14,7 @@ cat <<CONFIG_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/$robot/.setup_assistant
 moveit_setup_assistant_config:
   URDF:
     package: cob_hardware_config
-    relative_path: ROBOT/urdf/ROBOT.urdf.xacro
+    relative_path: robots/ROBOT/urdf/ROBOT.urdf.xacro
   SRDF:
     relative_path: config/ROBOT.srdf
 CONFIG_TEMPLATE
