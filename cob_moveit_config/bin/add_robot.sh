461b121019aa5e485bda2553f7febfaba6949483
ipa-fxm <felix.messmer@ipa.fhg.de>
2017-04-10 16:16:56 +0200

    restructure moveit config

--- cob_moveit_config/bin/add_robot.sh
+++ cob_moveit_config/bin/add_robot.sh
@@ -3,18 +3,18 @@
 robot=$1
 
 base=`rospack find cob_moveit_config`
-mkdir -p $base/$robot/config
+mkdir -p $base/robots/$robot/moveit/config
 
-cat <<SRDF_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/$robot/config/$robot.srdf
+cat <<SRDF_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/robots/$robot/moveit/config/$robot.srdf
 <robot name="ROBOT">
 </robot>
 SRDF_TEMPLATE
 
-cat <<CONFIG_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/$robot/.setup_assistant
+cat <<CONFIG_TEMPLATE  | sed "s/ROBOT/$robot/g" > $base/robots/$robot/moveit/.setup_assistant
 moveit_setup_assistant_config:
   URDF:
     package: cob_hardware_config
     relative_path: robots/ROBOT/urdf/ROBOT.urdf.xacro
   SRDF:
-    relative_path: config/ROBOT.srdf
+    relative_path: moveit/config/ROBOT.srdf
 CONFIG_TEMPLATE
